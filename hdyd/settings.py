460c63066a969ac5ba78fd65524b887c392eeafa
CrankyCat-Love-Coding <crankycatlovecoding@outlook.com>
2022-10-04 17:04:04 +0000

    reset static root

--- hdyd/settings.py
+++ hdyd/settings.py
@@ -158,7 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_ROOT = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -171,12 +172,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 if 'USE_AWS' in os.environ:
-    # Cache control
+        # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'hdyd'
     AWS_S3_REGION_NAME = 'eu-west-1'
