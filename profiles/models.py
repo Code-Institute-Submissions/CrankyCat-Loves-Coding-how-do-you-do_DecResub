f69fcf24601c7b7578b0a547d7a80c18d0632a3a
CrankyCat-Love-Coding <crankycatlovecoding@outlook.com>
2022-10-10 10:03:15 +0000

    clean codes

--- profiles/models.py
+++ profiles/models.py
@@ -10,8 +10,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50, null=True, blank=False)
 
-    # def __str__(self):
-    #     return self.user
 
 
 @receiver(post_save, sender=User)
